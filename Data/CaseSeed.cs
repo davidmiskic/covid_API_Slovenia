3ed99d5962843179261c0167c77fb9c89e9670b9
DavidX6 <david.miskic@gmail.com>
2021-09-30 15:24:00 +0200

    start csv parsing

--- Data/CaseSeed.cs
+++ Data/CaseSeed.cs
@@ -2,7 +2,10 @@ using CovidAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 
 public class DataGenerator
 {
@@ -11,23 +14,58 @@ public class DataGenerator
         using (var context = new CaseContext(
             serviceProvider.GetRequiredService<DbContextOptions<CaseContext>>()))
         {
-            // Look for any board games.
             if (context.Cases.Any())
             {
                 return;   // Data was already seeded
             }
+            // debug: Directory.SetCurrentDirectory(@"C:....");
+            var client = new WebClient();
+            client.DownloadFile("https://raw.githubusercontent.com/sledilnik/data/master/csv/region-cases.csv", "db.csv");
+            
+            StreamReader file = new StreamReader("db.csv");
+            string line;
+            int count = 0;
+            List<Case> CSVCases = new List<Case>();
 
-            context.Cases.AddRange(
+            while ((line = file.ReadLine()) != null)
+            {
+                if (count == 0) continue;
+                count++;
+
+                string[] fields = line.Split(',');
+                CSVCases.Add(
                 new Case
                 {
-                    Id = "Test Post 1caseseed",
-                    Region = "This is my standard post for testing"
-                },
-                new Case
-                {
-                    Id = "Test Post 2caseseed",
-                    Region = "This is my standard post for testing"
+                    Id = fields[0],
+                    Region = fields[1]
                 });
+
+                if (count > 10) break;
+
+                //decimal amount = decimal.Parse(fields[5].ToString()) * decimal.Parse("0.01");
+
+
+                //cmd.Parameters.AddWithValue("@Payor", Security.RemoveSpecialCharacters(fields[1].ToString()).Trim());
+                //cmd.Parameters.AddWithValue("@Amount", amount); Security.RemoveSpecialCharacters(fields[8].ToString()).Trim());
+                //cmd.Parameters.AddWithValue("@Account", Security.RemoveSpecialCharacters(fields[2].ToString()));
+                //cmd.Parameters.AddWithValue("@Serial", Security.RemoveSpecialCharacters(fields[0].ToString()).Trim().Trim());
+
+            }
+
+            context.Cases.AddRange(CSVCases);
+
+
+            //context.Cases.AddRange(
+            //    new Case
+            //    {
+            //        Id = "Test Post 1caseseed",
+            //        Region = "This is my standard post for testing"
+            //    },
+            //    new Case
+            //    {
+            //        Id = "Test Post 2caseseed",
+            //        Region = "This is my standard post for testing"
+            //    });
 
             context.SaveChanges();
         }
